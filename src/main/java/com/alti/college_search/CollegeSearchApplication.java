369069af0c67550e5c7058426076192b3bd38bb8
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2019-12-19 09:37:33 -0600

    First Commit

---
+++ src/main/java/com/alti/college_search/CollegeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.alti.college_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alti/college_search/CollegeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alti.college_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
