23b2ab1b0559e503b4752eb142c1cc34c4274be4
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2019-12-19 21:33:30 -0600

    MVP is done

--- src/main/java/com/alti/college_search/service/MapsService.java
+++ src/main/java/com/alti/college_search/service/MapsService.java
@@ -1,18 +1,10 @@
 package com.alti.college_search.service;
 
-import com.alti.college_search.models.Metadata;
 import com.alti.college_search.models.ResponseObj;
-import com.alti.college_search.models.School;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class MapsService {
